42167ba3256748cc5f337aaae77e22a0e800d743
Sahil Saudagar <codewithsahil.dev@gmail.com>
2025-02-12 03:11:25 +0530

    add constant base api for assets

--- src/shared/Constants.tsx
+++ src/shared/Constants.tsx
@@ -1,9 +1,11 @@
 interface Constants {
-  BASE_URL: string;
+  BASE_URL_MIN_API: string;
+  BASE_URL_DATA_API: string;
   BASE_IMAGE_URL: string;
 }
 
 export const Constants: Constants = {
-  BASE_URL: "https://min-api.cryptocompare.com/data",
+  BASE_URL_MIN_API: "https://min-api.cryptocompare.com/data",
+  BASE_URL_DATA_API: "https://data-api.cryptocompare.com/asset/v1/data",
   BASE_IMAGE_URL: "https://www.cryptocompare.com",
 };
